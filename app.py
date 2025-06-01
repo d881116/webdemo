1ab43316e04bf21b4f1ef067c85968187b3743f8
d881116 <80759615+d881116@users.noreply.github.com>
2025-06-02 02:54:16 +0800

    Update app.py

--- app.py
+++ app.py
@@ -32,5 +32,10 @@ def model2_predict(text):
 def model3_predict(text):
     return [f"模型3 回傳: {text}"]
 
+'''if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=5000, debug=True)'''
+
+
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=5000, debug=True)
+    port = int(os.environ.get('PORT', 5000))
+    app.run(host='0.0.0.0', port=port)
